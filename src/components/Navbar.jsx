8c4efb4557a1d7fe8531e5348b25f8388e8d1934
Ahqaf Ali <ahqafaliofficial@gmail.com>
2025-07-21 20:31:48 +0530

    made basic navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,19 @@
 function Navbar() {
     return (
         <div>
-            <h1>Navbar</h1>
+            <div className="flex justify-between items-center bg-black text-white p-4 ">
+                <div className="flex items-center">
+                    OPENGEEK
+            
+                </div>
+                <div className="flex items-center gap-4">
+                    <a href="#">Home</a>
+                    <a href="#">About</a>
+                    <a href="#">Services</a>
+                    <a href="#">Contact</a>
+                </div>
+            </div>
+            
         </div>
     )
 }
